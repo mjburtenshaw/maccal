0f95b1d80306204459e6852df5bef706a7124552
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2023-08-25 16:48:42 -0600

    feat: Add Google Calendar quickstart

---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+)
+
+func main() {
+
+    // Get the current working directory
+    cwd, err := os.Getwd()
+    if err != nil {
+        log.Fatal("Unable to get current working directory", err)
+    }
+
+    // Load credentials from the downloaded JSON file
+    credentialsFile := fmt.Sprintf("%s/secrets/google.credentials.json", cwd)
+    b, err := os.ReadFile(credentialsFile)
+    if err != nil {
+        log.Fatalf("Unable to read client secret file: %v", err)
+    }
+
+    // Create OAuth2 config from JSON
+    config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+    if err != nil {
+        log.Fatalf("Unable to parse client secret file to config: %v", err)
+    }
+
+    config.RedirectURL = "http://localhost:8080/oauth2callback"
+
+    tokenFile := fmt.Sprintf("%s/secrets/google.token.json", cwd)
+
+    http.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+
+		tok, err := config.Exchange(context.Background(), code)
+		if err != nil {
+				http.Error(w, fmt.Sprintf("Unable to exchange code for token: %v", err), http.StatusInternalServerError)
+				return
+		}
+	
+		// Save the token to a file (or your preferred storage)
+		if err := saveToken(tokenFile, tok); err != nil {
+				http.Error(w, fmt.Sprintf("Unable to save token: %v", err), http.StatusInternalServerError)
+				return
+		}
+	
+		// Display a success message
+		w.Write([]byte("Authentication successful! You can close this window now."))
+        
+        // TODO: continue the process
+	})
+
+	// Start the HTTP server
+	go func() {
+        log.Fatal(http.ListenAndServe(":8080", nil))
+    }()
+
+    // Create an OAuth2 token
+    tok, err := getToken(config, tokenFile)
+    if err != nil {
+        log.Fatalf("Unable to get token: %v", err)
+    }
+
+    // Create a new Calendar client with the token
+    client := config.Client(context.Background(), tok)
+
+    // Create a Calendar service
+    srv, err := calendar.New(client)
+    if err != nil {
+        log.Fatalf("Unable to create Calendar service: %v", err)
+    }
+
+    // Use the srv object to interact with the Google Calendar API
+    // For example, you can list calendar events using srv.Events.List()
+    events := srv.Events.List("primary")
+    fmt.Println(events)
+
+    // Wait for a signal to stop the program (e.g., Ctrl+C)
+    // This is just an example. You might have other logic to control program termination.
+    // select {}
+}
+
+func getToken(config *oauth2.Config, tokenFile string) (*oauth2.Token, error) {
+    tok, err := tokenFromFile(tokenFile)
+    if err == nil {
+        return tok, nil
+    }
+    tok = getTokenFromWeb(config)
+    if err := saveToken(tokenFile, tok); err != nil {
+        return nil, err
+    }
+    return tok, nil
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+    f, err := os.Open(file)
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+    tok := &oauth2.Token{}
+    err = json.NewDecoder(f).Decode(tok)
+    return tok, err
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+    authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+    fmt.Printf("Go to the following link in your browser, then type the "+
+        "authorization code: \n%v\n", authURL)
+
+    var authCode string
+    if _, err := fmt.Scan(&authCode); err != nil {
+        log.Fatalf("Unable to read authorization code: %v", err)
+    }
+
+    tok, err := config.Exchange(context.Background(), authCode)
+    if err != nil {
+        log.Fatalf("Unable to retrieve token from web: %v", err)
+    }
+    return tok
+}
+
+func saveToken(file string, token *oauth2.Token) error {
+    fmt.Printf("Saving credential file to: %s\n", file)
+    f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+    return json.NewEncoder(f).Encode(token)
+}
+
+
+// Helper functions to manage tokens (tokenFromFile, getTokenFromWeb, saveToken)
+// are not shown here for brevity.
+
