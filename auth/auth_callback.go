d91af7cf9cd7b1559a5f50f00851cea5bfa6d5ca
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2023-08-31 16:07:31 -0600

    refactor: Move callback server

---
+++ auth/auth_callback.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/oauth2"
+)
+
+func StartAuthCallbackServer(config *oauth2.Config, tokenFile string) {
+    http.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+
+		tok, err := config.Exchange(context.Background(), code)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("⛔️ Unable to exchange code for token: %v", err), http.StatusInternalServerError)
+			return
+	}
+	
+		// Save the token to a file (or your preferred storage)
+		if err := SaveToken(tokenFile, tok); err != nil {
+			http.Error(w, fmt.Sprintf("⛔️ Unable to save token: %v", err), http.StatusInternalServerError)
+			return
+		}
+	
+		// Display a success message
+		w.Write([]byte("🎉 Authentication successful! You can close this window now."))
+        
+        // TODO: continue the process
+	})
+
+	// Start the HTTP server
+	go func() {
+        log.Fatal(http.ListenAndServe(":8080", nil))
+    }()
+}
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"golang.org/x/oauth2/google"
@@ -39,31 +38,7 @@ func main() {
 
     tokenFile := fmt.Sprintf("%s/secrets/google.token.json", cwd)
 
-    http.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Query().Get("code")
-
-		tok, err := config.Exchange(context.Background(), code)
-		if err != nil {
-				http.Error(w, fmt.Sprintf("⛔️ Unable to exchange code for token: %v", err), http.StatusInternalServerError)
-				return
-		}
-	
-		// Save the token to a file (or your preferred storage)
-		if err := auth.SaveToken(tokenFile, tok); err != nil {
-				http.Error(w, fmt.Sprintf("⛔️ Unable to save token: %v", err), http.StatusInternalServerError)
-				return
-		}
-	
-		// Display a success message
-		w.Write([]byte("🎉 Authentication successful! You can close this window now."))
-        
-        // TODO: continue the process
-	})
-
-	// Start the HTTP server
-	go func() {
-        log.Fatal(http.ListenAndServe(":8080", nil))
-    }()
+    auth.StartAuthCallbackServer(config, tokenFile)
 
     // Create an OAuth2 token
     tok, err := auth.GetToken(config, tokenFile)
